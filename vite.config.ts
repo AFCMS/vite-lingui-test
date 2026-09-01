bf75416c22d6be6cc9bdef8055a9fabed6602e2a
AFCMS <afcm.contact@gmail.com>
2026-09-01 22:03:08 +0200

    OXC based React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { lingui, linguiTransformerBabelPreset } from "@lingui/vite-plugin";
 import babel from "@rolldown/plugin-babel";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({ compiler: true }),
     lingui(),
-    babel({ presets: [reactCompilerPreset(), linguiTransformerBabelPreset()] }),
+    babel({ presets: [linguiTransformerBabelPreset()] }),
   ],
 });
